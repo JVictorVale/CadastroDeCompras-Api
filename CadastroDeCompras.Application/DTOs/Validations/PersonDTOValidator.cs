ab58398fa1688d1e318b6018e8a5828eb92e4570
victorvale <joaovictorvale06@gmail.com>
2023-09-05 16:43:48 -0300

    Criando a DTO de pessoa e suas validações

---
+++ CadastroDeCompras.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,26 @@
+using System.Data;
+using FluentValidation;
+
+namespace CadastroDeCompras.Application.DTOs.Validations
+{
+    public class PersonDTOValidator : AbstractValidator<PersonDTO>
+    {
+        public PersonDTOValidator()
+        {
+            RuleFor(x => x.Document)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Document deve ser informado!");
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Name deve ser informado!");
+
+            RuleFor(x => x.Phone)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Phone deve ser informado!");
+        }
+    }
+}

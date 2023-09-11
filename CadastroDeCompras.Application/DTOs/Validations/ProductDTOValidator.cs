f35a34e3184561abb13488b65d59212a5aebafc3
victorvale <joaovictorvale06@gmail.com>
2023-09-11 14:29:26 -0300

    Criando a DTO de produto e suas validações

---
+++ CadastroDeCompras.Application/DTOs/Validations/ProductDTOValidator.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+
+namespace CadastroDeCompras.Application.DTOs.Validations
+{
+
+    public class ProductDTOValidator : AbstractValidator<ProductDTO>
+    {
+        public ProductDTOValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Nome deve ser informado!");
+
+            RuleFor(x => x.CodErp)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("CodErp deve ser informado!");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Preço deve ser maior do que zero!");
+        }
+    }
+}
